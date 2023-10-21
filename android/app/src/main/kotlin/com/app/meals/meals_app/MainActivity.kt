01e846f32312717f3af027e9bb5e88d2025b76fd
s13g <ayflix0@gmail.com>
2023-10-21 22:48:17 +0100

    wokring on meals

---
+++ android/app/src/main/kotlin/com/app/meals/meals_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.meals.meals_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/categories.dart
@@ -0,0 +1,54 @@
+import 'package:meals_app/models/category.dart';
+import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    // check whether the meal categories contain the category id
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    // or Navigator.of(context).push(route)
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            MealsScreen(title: category.title, meals: filteredMeals),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        // number of columns i wanna have = 2
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2, // impacts the sizing of the gridview items
+            crossAxisSpacing: 20, // some spacing between columns
+            mainAxisSpacing:
+                20 // spacing of 20 pixels horizontally and vertically between grid items
+            ),
+        children: [
+          // use this for loop as an alternative for map
+          for (final category in availableCategories)
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                })
+        ],
+      ),
+    );
+  }
+}
