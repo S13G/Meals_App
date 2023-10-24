b7a934f850082529aac7109d5aafa0da05c1476d
s13g <ayflix0@gmail.com>
2023-10-24 22:28:39 +0100

    favorite feature

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,13 @@
 import 'package:meals_app/models/category.dart';
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     // check whether the meal categories contain the category id
@@ -17,8 +19,11 @@ class CategoriesScreen extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) =>
-            MealsScreen(title: category.title, meals: filteredMeals),
+        builder: (context) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
+        ),
       ),
     );
   }
