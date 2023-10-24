3059847b4781242770e53f96bd1fe63b29d72eae
s13g <ayflix0@gmail.com>
2023-10-24 23:19:02 +0100

    adding side drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -33,12 +34,12 @@ class _TabsScreenState extends State<TabsScreen> {
       setState(() {
         _favoriteMeals.remove(meal);
       });
-      _showInfoMessage('Meal is no longer a favorite')
+      _showInfoMessage('Meal is no longer a favorite');
     } else {
       setState(() {
         _favoriteMeals.add(meal);
       });
-      _showInfoMessage('Marked as a favorite')
+      _showInfoMessage('Marked as a favorite');
     }
   }
 
@@ -67,6 +68,7 @@ class _TabsScreenState extends State<TabsScreen> {
         appBar: AppBar(
           title: Text(activePageTitle),
         ),
+        drawer: const MainDrawer(),
         body: activePage,
         bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
