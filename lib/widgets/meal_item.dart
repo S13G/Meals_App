6293bd2d319a9bc86597cf4e632607f22caa2cd6
s13g <ayflix0@gmail.com>
2023-10-27 09:12:59 +0100

    implemented image screens animation

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -38,12 +38,15 @@ class MealItem extends StatelessWidget {
         //Stack to put widgets on top each other(relative to each other) not like column
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
